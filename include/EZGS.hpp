2787e7aea278f7074dae168809f4278d9e1ffc82
yossyi <39930174+nkue-yst@users.noreply.github.com>
2020-08-15 15:04:30 +0900

    Update Texture class

--- include/EZGS.hpp
+++ include/EZGS.hpp
@@ -2,16 +2,19 @@
  * @file EZGS.hpp
  * @brief include用ヘッダファイル
  * @author Yoshito Nakaue
- * @date 2020/08/12
+ * @date 2020/08/15
  */
 
 #pragma once
 
 #include <EZGS/Actor.hpp>
 #include <EZGS/Component.hpp>
+#include <EZGS/DrawComponent.hpp>
 #include <EZGS/Math.hpp>
+#include <EZGS/MoveComponent.hpp>
 #include <EZGS/Shader.hpp>
 #include <EZGS/System.hpp>
+#include <EZGS/Texture.hpp>
 #include <EZGS/VertexArray.hpp>
 
 using namespace ezgs;
--- src/Texture.cpp
+++ src/Texture.cpp
@@ -1,9 +1,13 @@
 /**
  * @author Yoshito Nakaue
- * @date 2020/08/13
+ * @date 2020/08/15
  */
 
 #include <EZGS/Texture.hpp>
+#include <GL/glew.h>
+#include <SDL.h>
+#include <SDL_image.h>
+
 
 namespace ezgs
 {
@@ -22,8 +26,42 @@ namespace ezgs
 
     int Texture::LoadImage(const std::string& file_name)
     {
-        int channels_num = 0;
-        unsigned char* image = 
+        SDL_Surface* image = IMG_Load(file_name.c_str());
+
+        if (image == nullptr)
+        {
+            SDL_Log("Failed to load image %s.", file_name.c_str());
+            return 1;
+        }
+
+        this->width_  = image->w;
+        this->height_ = image->h;
+
+        int format = GL_RGB;
+        if (image->format->BytesPerPixel == 4)
+            format = GL_RGBA;
+
+        glGenTextures(1, &texture_id_);
+        glBindTexture(GL_TEXTURE_2D, texture_id_);
+
+        glTexImage2D(
+            GL_TEXTURE_2D,
+            0,
+            format,
+            width_,
+            height_,
+            0,
+            format,
+            GL_UNSIGNED_BYTE,
+            image->pixels
+        );
+
+
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        return 0;
     }
 
     void Texture::UnloadImage()
